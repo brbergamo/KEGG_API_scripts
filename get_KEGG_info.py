f8e298a7f5d99cd1dc472344a954b4a4fdc05a10
beatrizrb <beatrizrbergamo@gmail.com>
2024-06-08 18:31:00 -0300

    updating python module

--- get_KEGG_info.py
+++ get_KEGG_info.py
@@ -4,31 +4,35 @@ import argparse
 import re
 from rich.progress import Progress
 import pathlib
+import urllib
+from termcolor import colored
+import sys
 
 
 def main():
     args = arguments_parse()
     ids = args.ids_list.split(",")
-    data = []
+    list_data = []
 
     with Progress() as progress:
         task1 = progress.add_task("[cyan]Retrieving info...", total=len(ids))
-
         while not progress.finished:
             for id in ids:
-                info = get_info(id)
-                data.append(info)
+                data = get_info(id)
+                filtered_data = filter_info(data)
+                list_data.append(filtered_data)
                 progress.update(task1, advance=1)
 
     path = pathlib.Path("./KEGG_info_results")
     path.mkdir(exist_ok=True)
-    to_df(data, args.name, path)
+    to_df(list_data, args.name, path)
 
 
 def arguments_parse():
-    parser = argparse.ArgumentParser(prog="KEGG Info",
-                                     description="python module to help get"
-                                     "IDs translated with KEGG info")
+    parser = argparse.ArgumentParser(
+        prog="KEGG Info",
+        description="python module to help get" "IDs translated with KEGG info",
+    )
     parser.add_argument("ids_list")
     parser.add_argument("--name")
     args = parser.parse_args()
@@ -36,12 +40,26 @@ def arguments_parse():
 
 
 def get_info(id):
-    info = REST.kegg_get(id).read()
-    if "DESCRIPTION" in info:
-        return (list(re.search(r"NAME\s+(.+)\nDESCRIPTION\s+(.+)\nCLASS\s+(.+)",
-                               info).groups()))
+    try:
+        return REST.kegg_get(id).read()
+    except urllib.error.HTTPError as e:
+        if e.code == 400:
+            print(
+                colored(
+                    "ID invalid. Are you sure it is correct? Please, input a valid ID.",
+                    "magenta",
+                )
+            )
+        sys.exit()
 
-    return (list(re.search(r"NAME\s+(.+)\n", info).groups()) + [None, None])
+
+def filter_info(info):
+    if "DESCRIPTION" in info:
+        return list(
+            re.search(r"NAME\s+(.+)\nDESCRIPTION\s+(.+)\nCLASS\s+(.+)", info).groups()
+        )
+
+    return list(re.search(r"NAME\s+(.+)\n", info).groups()) + [None, None]
 
 
 def to_df(data, name, path):
